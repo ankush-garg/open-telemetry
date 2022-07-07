c3e8a9de9dd22e1ac4c54625862514c3c1cd7a1d
ankush-garg <agarg.punjabi@gmail.com>
2022-07-07 16:10:55 +0530

    fetch

--- app1.js
+++ app1.js
@@ -3,12 +3,15 @@
 require('./trace')('app-1')
 
 const express = require('express')
+const fetch = require('node-fetch')
 
 const PORT = process.env.PORT || '8080'
 const app = express()
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World App 1')
+app.get('/hello', async (req, res, next) => {
+  const response = await fetch('http://localhost:8081/hello')
+  const body = await response.text()
+  res.send('Hello World App 1 received - ' + body)
 })
 
 app.listen(parseInt(PORT, 10), () => {
